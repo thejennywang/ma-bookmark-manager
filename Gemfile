7ffbbcc8d0036beefecfd55a0b336c699ee7f714
thejennywang <thejennywang@gmail.com>
2014-07-28 16:13:31 +0100

    Added database

--- Gemfile
+++ Gemfile
@@ -1,4 +1,13 @@
 source 'https://rubygems.org'
 
+gem 'sinatra'
 gem 'data_mapper'
 gem 'dm-postgres-adapter'
+gem 'shotgun'
+gem 'capybara'
+gem 'cucumber'
+gem 'cucumber-sinatra'
+
+group :development, :test do
+	gem 'rspec'
+end
---
+++ lib/server.rb
@@ -0,0 +1,13 @@
+require 'data_mapper'
+
+env = ENV["RACK_ENV"] || "development"
+# we're telling datamapper to use a postgres database on localhost. The name will be 'bookmaker_manager_test' or 'bookmaker_manager_development" depending on the environment'
+DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
+
+require './lib/link' # this need to be done after datamapper is initialized
+
+# After declaring your models, you shold finalize them
+DataMapper.finalize
+
+#However, the database tables don't exist yet. Let's tell datamapper to create them.
+DataMapper.auto_upgrade!
