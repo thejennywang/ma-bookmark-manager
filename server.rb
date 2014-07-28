137ffbe6ed627c796e4ca6f95ac4704be0eb498d
thejennywang <thejennywang@gmail.com>
2014-07-28 18:19:03 +0100

    Added input for submitting a new link

--- server.rb
+++ server.rb
@@ -15,11 +15,18 @@ DataMapper.auto_upgrade!
 
 class BookmarkManager < Sinatra::Base
 
-get '/' do
-	# "Makers Academy"
-	@links = Link.all
-	erb :index
-end
+	get '/' do
+		# "Makers Academy"
+		@links = Link.all
+		erb :index
+	end
+
+	post '/links' do
+		url = params["url"]
+		title = params["title"]
+		Link.create(:url => url, :title => title)
+		redirect to('/')
+	end
 
 	#start the server if ruby file executed directly
 	run! if app_file ==$0
